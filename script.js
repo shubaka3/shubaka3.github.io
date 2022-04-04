4df76c1225641c7b0bfbc8e4d75a629666dfb629
shubaka3 <96128376+shubaka3@users.noreply.github.com>
2022-04-04 21:01:28 +0700

    upload

--- script.js
+++ script.js
@@ -1,85 +1,133 @@
-const backgrounds=document.getElementById("background")
-            document.getElementById("btn21").onclick = function () {
-                document.getElementById("header").style.display = 'none';
-                document.getElementById("mainslider").style.display = 'block';
-                document.getElementById("slider2").style.display = 'none';
-                backgrounds.style.background="url(img/board.jpg) top center / cover no-repeat" 
-            };
+// full screen
+// zoom btn
+var html = document.querySelector('html')
+
+
+// ============================
+// range input styling
+document.querySelectorAll("input[type=range]").forEach(function(item){
+    item.addEventListener("input",function() {
+            var value = (this.value-this.min)/(this.max-this.min)*100
+            // this.style.background = 'linear-gradient(to right, #f3a952 0%, #f3a952 ' + value + '%, transparent ' + value + '%, transparent 100%)'
+            this.style.background = value + '%, transparent ' + value + '%, transparent 100%)'
+    })
+})
+
+
+// ============================================
+// MODAL
+
+//=================== Volume========================
+// main volume
+let mainVolume = document.querySelector('#volume-bar')
+mainVolume.addEventListener('input', function(){
+    mainAudio.volume = this.value/100
+})
+// effect volume
+// traffic
+document.querySelector('#traffic-volume').addEventListener('input', function(){
+    document.querySelector('#traffic-audio').play()
+    document.querySelector('#traffic-audio').volume = this.value/100
+})
+// rain
+document.querySelector('#rain-volume').addEventListener('input', function(){
+    document.querySelector('#rain-audio').play()
+    document.querySelector('#rain-audio').volume = this.value/100
+})
+
+// fireplace
+document.querySelector('#fireplace-volume').addEventListener('input', function(){
+    document.querySelector('#fireplace-audio').play()
+    document.querySelector('#fireplace-audio').volume = this.value/100
+})
+// campfire
+document.querySelector('#campfire-volume').addEventListener('input', function(){
+    document.querySelector('#campfire-audio').play()
+    document.querySelector('#campfire-audio').volume = this.value/100
+})
+// forest
+document.querySelector('#forest-volume').addEventListener('input', function(){
+    document.querySelector('#forest-audio').play()
+    document.querySelector('#forest-audio').volume = this.value/100
+})
+// forest-rain
+document.querySelector('#forest-rain-volume').addEventListener('input', function(){
+    document.querySelector('#forest-rain-audio').play()
+    document.querySelector('#forest-rain-audio').volume = this.value/100
+})
+// waves
+document.querySelector('#waves-volume').addEventListener('input', function(){
+    document.querySelector('#waves-audio').play()
+    document.querySelector('#waves-audio').volume = this.value/100
+})
+// fan
+document.querySelector('#fan-volume').addEventListener('input', function(){
+    document.querySelector('#fan-audio').play()
+    document.querySelector('#fan-audio').volume = this.value/100
+})
+// storm
+document.querySelector('#storm-volume').addEventListener('input', function(){
+    document.querySelector('#storm-audio').play()
+    document.querySelector('#storm-audio').volume = this.value/100
+})
+// river
+document.querySelector('#river-volume').addEventListener('input', function(){
+    document.querySelector('#river-audio').play()
+    document.querySelector('#river-audio').volume = this.value/100
+})
+// bird
+document.querySelector('#bird-volume').addEventListener('input', function(){
+    document.querySelector('#bird-audio').play()
+    document.querySelector('#bird-audio').volume = this.value/100
+})
+// people
+document.querySelector('#people-volume').addEventListener('input', function(){
+    document.querySelector('#people-audio').play()
+    document.querySelector('#people-audio').volume = this.value/100
+})
+// wind
+document.querySelector('#wind-volume').addEventListener('input', function(){
+    document.querySelector('#wind-audio').play()
+    document.querySelector('#wind-audio').volume = this.value/100
+})
+// ocean
+document.querySelector('#ocean-volume').addEventListener('input', function(){
+    document.querySelector('#ocean-audio').play()
+    document.querySelector('#ocean-audio').volume = this.value/100
+})
+// blizzard
+document.querySelector('#blizzard-volume').addEventListener('input', function(){
+    document.querySelector('#blizzard-audio').play()
+    document.querySelector('#blizzard-audio').volume = this.value/100
+})
+
+
+
+
+// ====================== modifier modifier icons modifier event
+// display none when click outside
+document.querySelector('html').onclick = function(){
+    if(!document.querySelector('#settings-board').classList.contains('display-none')){
+        document.querySelector('#settings-board').classList.add('display-none')
+        document.querySelector('#settings').classList.remove('modifier-icon--active')
+    }
  
-            document.getElementById("btn1").onclick = function () {
-                document.getElementById("header").style.display = 'block';
-                document.getElementById("mainslider").style.display = 'none';
-                document.getElementById("slider2").style.display = 'none';
-                backgrounds.style.background="url(img/5923902.jpg) top center / cover no-repeat" 
+}
+// settings board
+document.querySelector('#nav3').addEventListener("click", function(e){
+    e.stopPropagation()
+    document.querySelector('#nav3').classList.toggle('modifier-icon--active')
+    document.querySelector('#settings-board').classList.toggle('display-none')
 
-            };
-            document.getElementById("btn22").onclick = function () {
-                document.getElementById("header").style.display = 'none';
-                document.getElementById("mainslider").style.display ='none';
-                document.getElementById("slider2").style.display = 'block';
-                backgrounds.style.background="url(img/backgroundslider2.jpg) top center / cover no-repeat" 
-
-            };
-            const backgroundtex=document.getElementById("backgroundlist")
-            const text1=document.getElementById("listtex1")
-            const text2=document.getElementById("listtex2")
-            const text3=document.getElementById("listtex3")
-            const text4=document.getElementById("listtex4")
-            const text5=document.getElementById("listtex5")
-            const alltext=document.getElementById("noidung")
-
-            document.getElementById("alllist1").onclick = function () {
-                //backgroundtex.style.background="url(img/background1.jpg) top center / cover no-repeat" 
-                text1.style.display="block";
-                text2.style.display="none";
-                text3.style.display="none";
-                text4.style.display="none";
-                text5.style.display="none";
-                
+})
 
 
-            };
-            document.getElementById("alllist2").onclick = function () {
-                //backgroundtex.style.background="url(img/background2.jpg) top center / cover no-repeat" 
-                text1.style.display="none";
-                text2.style.display="block";
-                text3.style.display="none";
-                text4.style.display="none";
-                text5.style.display="none";
-
-            };
-            document.getElementById("alllist3").onclick = function () {
-                //backgroundtex.style.background="url(img/bakcground3.jpg) top center / cover no-repeat" 
-                text1.style.display="none";
-                text2.style.display="none";
-                text3.style.display="block";
-                text4.style.display="none";
-                text5.style.display="none";
-                
 
 
-            };
-            document.getElementById("alllist4").onclick = function () {
-                //backgroundtex.style.background="url(img/background5.jpg) top center / cover no-repeat" 
-                text1.style.display="none";
-                text2.style.display="none";
-                text3.style.display="none";
-                text4.style.display="block";
-                text5.style.display="none";
+//focus mode 
 
-            };
-            document.getElementById("alllist5").onclick = function () {
-                //backgroundtex.style.background="url(img/background4.jpg) top center / cover no-repeat" 
-                text1.style.display="none";
-                text2.style.display="none";
-                text3.style.display="none";
-                text4.style.display="none";
-                text5.style.display="block";
-            };
 
-            
-            ////////////////////////<P>///////////////////////////////////
-            
-                
 
-           
+// share link drag function
+
+
