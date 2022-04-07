a6e2e11d9b9f82f81df5bac0b33025bce3672307
shubaka3 <96128376+shubaka3@users.noreply.github.com>
2022-04-07 22:13:16 +0700

    upload

---
+++ scripts/gMap.js
@@ -0,0 +1,21 @@
+var myCenter = new google.maps.LatLng(43.3907, -80.4060);
+
+function initialize() {
+var mapProp = {
+center:myCenter,
+zoom:12,
+scrollwheel:false,
+draggable:false,
+mapTypeId:google.maps.MapTypeId.ROADMAP
+};
+
+var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+var marker = new google.maps.Marker({
+position:myCenter,
+});
+
+marker.setMap(map);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
